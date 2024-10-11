f9b0dd5f7f31ccf449fc66911b268d0476fd8d07
EBOBOLA <81905322+EBOBOLA@users.noreply.github.com>
2024-10-11 17:23:55 +0700

    Delete proba.py

--- proba.py
+++
@@ -1,9 +0,0 @@
-class House():
-    def __init__(self, street, number):
-        self.street = street
-        self.number = number
-    def build(self):
-        print("дом на " + self.street + "номер" + self.number)
-House1 = House("топ",20)
-House2 = House("топ",21)
-print(House2.number)
